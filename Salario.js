3a5760c9e3bff923e060726d17d163c42e7f1a71
Welingtonrsilva <147186897+Welingtonrsilva@users.noreply.github.com>
2024-01-26 16:31:40 -0300

    Nome de Funcionários e  Média Salários

---
+++ Salario.js
@@ -0,0 +1,28 @@
+Inicio
+   // Seção de Comandos, procedimento, funções, operadores, etc...
+   escreval("Insira o nome funcionario 1")
+   leia(funcionario1)
+   escreval("Insira o primeiro salario:")
+   leia(salario1)
+   escreval("Insira o nome funcionario 2")
+   leia(funcionario2)
+   escreval("Insira o segundo salario:")
+   leia(salario2)
+   escreval("Insira o nome funcionario 3")
+   leia(funcionario3)
+   escreval("Insira o terceior salario:")
+   leia(salario3)
+   escreval("Insira o nome funcionario 4")
+   leia(funcionario4)
+   escreval("Insira o quarto salario:")
+   leia(salario4)
+   escreval("Insira o nome funcionario 5")
+   leia(funcionario5)
+   escreval("Insira o quinto salario:")
+   leia(salario5)
+
+
+   resultado <- (salario1+salario2+salario3+salario4+salario5)/5
+   escreval("A média dos salários dos funcionários è:", resultado)
+
+Fimalgoritmo
